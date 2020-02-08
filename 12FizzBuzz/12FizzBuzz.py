c80d7a5716d82772b3fc8f6ad9beaf7c1a20dc36
Norbert <54719382+NorbertCode@users.noreply.github.com>
2020-02-08 15:32:47 +0100

    Update 12FizzBuzz.py
    Added the comments

--- 12FizzBuzz/12FizzBuzz.py
+++ 12FizzBuzz/12FizzBuzz.py
@@ -1,9 +1,13 @@
 for x in range(1,100):
+    #Declare a string
     strX = ""
+    #If x is a multiple of 3 concatenate strX with "Fizz"
     if x % 3 == 0:
         strX += "Fizz"
+    #If x is a multiple of 3 concatenate strX with "Buzz"
     if x % 5 == 0:
         strX += "Buzz"
+    #If it's neither set strX to x
     if strX == "":
         strX = str(x)
 
